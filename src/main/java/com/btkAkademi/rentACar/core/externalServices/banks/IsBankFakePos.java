469e4bfb857a18a0ae5317a71baf409fa6ae5f52
kodluyoruz <82022374+mertyalcin-code@users.noreply.github.com>
2022-01-28 17:09:03 +0300

    fix

--- src/main/java/com/btkAkademi/rentACar/core/externalServices/banks/IsBankFakePos.java
+++ src/main/java/com/btkAkademi/rentACar/core/externalServices/banks/IsBankFakePos.java
@@ -3,6 +3,6 @@ package com.btkAkademi.rentACar.core.externalServices.banks;
 public class IsBankFakePos {
 	
 	public boolean posService(String cardNo,String day,String mounth,String cVV,double amount) {
-		return true;
+		return false;
 	}
 }
